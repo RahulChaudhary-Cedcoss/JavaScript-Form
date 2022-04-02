4ee03da446bac770e9da89d245ef3bff72151265
Rahul <rahulchaudhary@cedcoss.com>
2022-04-02 14:10:38 +0530

    New Commit

--- src/main.js
+++ src/main.js
@@ -6,47 +6,47 @@
         var wt = parseInt(wt);
         wt = document.getElementById("wt").value;
     if (Age>=5 && Age <=7){
-        if(wt>15){
-            alert("Under");
+        if(wt<15){
+            alert("Under Weight");
         }
-        else if (wt==15 && wt==20) {
-            alert("Average");
+        else if (wt>=15 && wt<=20) {
+            alert("Average Weight");
         }
-        else if(wt<20) {
-            alert("Over");
+        else if(wt>20) {
+            alert("Over Weight");
         }
     }
     if (Age>=8 && Age<=10) {
-        if(wt>21){
-            alert("Under");
+        if(wt<21){
+            alert("Under Weight");
         }
-        else if (wt==21 && wt==25) {
-            alert("Average");
+        else if (wt>=21 && wt<=25) {
+            alert("Average Weight");
         }
-        else if(wt<25) {
-            alert("Over");
+        else if(wt>25) {
+            alert("Over Weight");
         }
     }
     if (Age>=11 && Age<=15) {
-        if(wt>26){
-            alert("Under");
+        if(wt<26){
+            alert("Under Weight");
         }
-        else if (wt==26 && wt==30) {
-            alert("Average");
+        else if (wt>=26 && wt<=30) {
+            alert("Average Weight");
         }
-        else if(wt<30) {
-            alert("Over");
+        else if(wt>30) {
+            alert("Over Weight");
         }
     }
     if (Age>=16 && Age<=20) {
-        if(wt>31){
-            alert("Under");
+        if(wt<31){
+            alert("Under Weight");
         }
-        else if (wt==31 && wt==40) {
-            alert("Average");
+        else if (wt>=31 && wt<=40) {
+            alert("Average Weight");
         }
-        else if(wt<40) {
-            alert("Over");
+        else if(wt>40) {
+            alert("Over Weight");
         }
     }
     else {
